4dce89abbc6af2ec9b6cbe83f0d6e945a84259a6
Jimvid <victor@jimvid>
2023-06-06 21:55:35 +0200

    Title: Rework size to reflect font-size and add option for heading tag in 'heading' prop instead.

--- src/components/Title.svelte
+++ src/components/Title.svelte
@@ -1,25 +1,36 @@
 <script lang="ts">
-	type Size = 'h1' | 'h2' | 'h3' | 'h4' | 'h5';
-	export let size: Size = 'h1';
+	type Size = 'xs' | 'sm' | 'md' | 'lg' | 'xl';
+	type Heading = 'h1' | 'h2' | 'h3' | 'h4' | 'h5';
+
+	export let size: Size = 'xl';
+	export let heading: Heading = 'h1';
 	export let style = '';
+
+	const sizes = {
+		xs: 'text-xl lg:text-2xl',
+		sm: 'text-2xl lg:text-3xl',
+		md: 'text-2xl md:text-3xl lg:text-4xl',
+		lg: 'text-3xl md:text-4xl lg:text-5xl',
+		xl: 'text-4xl md:text-5xl lg:text-6xl '
+	};
 </script>
 
-{#if size === 'h1'}
-	<h1 class="text-6xl font-bold {style}"><slot /></h1>
-{:else if size === 'h2'}
-	<h2 class="text-5xl font-bold {style}">
+{#if heading === 'h1'}
+	<h1 class="font-bold {sizes[size]} {style}"><slot /></h1>
+{:else if heading === 'h2'}
+	<h2 class=" font-bold {sizes[size]} {style}">
 		<slot />
 	</h2>
-{:else if size === 'h3'}
-	<h3 class="text-xl {style}">
+{:else if heading === 'h3'}
+	<h3 class=" font-semibold {sizes[size]} {style}">
 		<slot />
 	</h3>
-{:else if size === 'h4'}
-	<h4 class={style}>
+{:else if heading === 'h4'}
+	<h4 class="font-semibold {sizes[size]} {style}">
 		<slot />
 	</h4>
-{:else if size === 'h5'}
-	<h5 class={style}>
+{:else if heading === 'h5'}
+	<h5 class="font-bold {sizes[size]} {style}">
 		<slot />
 	</h5>
 {/if}
