a0ab16f1ae51957edde20b65acdc0ff8bbf5b404
Jimvid <victor@jimvid>
2023-06-11 13:29:36 +0200

    Fix border color in Footer

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -4,7 +4,7 @@
 
 <div class="grid grid-cols-[1rem_minmax(0,_1fr)_1rem] text-surface-50 max-w-content m-auto">
 	<div
-		class="w-full px-12 py-8 col-start-2 col-end-3 font-semibold m-auto flex flex-col justify-center text-center sm:flex-row sm:justify-between border-x border-black"
+		class="w-full px-12 py-8 col-start-2 col-end-3 font-semibold m-auto flex flex-col justify-center text-center sm:flex-row sm:justify-between border-x border-accent"
 	>
 		<p class="mb-2 sm:mb-0">© 2023 Victor Jimvid</p>
 		<div class="flex gap-3 justify-center">
