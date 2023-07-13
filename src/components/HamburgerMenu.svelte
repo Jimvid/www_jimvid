1afa0ab3350b0ac74de7d8be73ac2c7f88b544ca
Jimvid <victor@jimvid>
2023-07-13 14:14:40 +0200

    Create first version of small device  menu and add to Navbar.

---
+++ src/components/HamburgerMenu.svelte
@@ -0,0 +1,47 @@
+<script>
+	import Icon from './Icon.svelte';
+
+	export let style = '';
+	$: isOpen = false;
+	const toggleMenu = () => (isOpen = !isOpen);
+</script>
+
+<div class={style}>
+	<button on:click={toggleMenu}>
+		<Icon icon="hamburger" />
+	</button>
+	{#if isOpen}
+		<div
+			class="p-8  fixed left-0 top-0 w-full bg-surface-50 dark:bg-surface-500 z-20 h-full flex flex-col justify-between"
+		>
+			<nav class="text-3xl font-bold">
+				<button class="text-sm flex gap-2 mb-8" on:click={toggleMenu}>
+					<Icon style="rotate-180" icon="arrow" size="sm" /> Go back
+				</button>
+				<ul class="flex flex-col gap-2">
+					<li>
+						<a on:click={() => (isOpen = false)} href="/">Home</a>
+					</li>
+					<li>
+						<a on:click={() => (isOpen = false)} href="/blog">Blog</a>
+					</li>
+					<li>
+						<a on:click={() => (isOpen = false)} href="/snippets">Snippets</a>
+					</li>
+					<li>
+						<a on:click={() => (isOpen = false)} href="/projects">Projects</a>
+					</li>
+				</ul>
+			</nav>
+
+			<div>
+				<p class="mb-2">You can find me here</p>
+				<div class="flex gap-3 ">
+					<Icon icon="mail" darkColor="surface-50" lightColor="surface-500" />
+					<Icon icon="linkedin" darkColor="surface-50" lightColor="surface-500" />
+					<Icon icon="github" darkColor="surface-50" lightColor="surface-500" />
+				</div>
+			</div>
+		</div>
+	{/if}
+</div>
--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -2,26 +2,30 @@
 	import { AppBar } from '@skeletonlabs/skeleton';
 	import { LightSwitch } from '@skeletonlabs/skeleton';
 	import Icon from './Icon.svelte';
+	import HamburgerMenu from './HamburgerMenu.svelte';
 </script>
 
 <div class="border-b border-accent">
 	<AppBar
-		class="max-w-content m-auto"
+		class="max-w-content m-auto border-t border-accent"
 		background="transparent"
 		padding="pt-0 pb-0"
 		gridColumns="grid-cols-[0rem_minmax(0,_1fr)_0rem]"
 		slotDefault="col-start-2 col-end-3 flex justify-between items-center border-x border-accent"
 	>
-		<a class="ml-8" href="/">
-			<Icon icon="logo" size="md" />
-		</a>
-		<div class="flex border-l border-accent items-center">
-			<div class="flex px-8 gap-4 font-semibold p-4">
+		<div class="ml-4">
+			<a class="hidden sm:inline" href="/">
+				<Icon icon="logo" size="md" />
+			</a>
+			<HamburgerMenu style="sm:hidden" />
+		</div>
+		<div class="flex sm:border-l border-accent items-center">
+			<div class="hidden sm:flex px-8 gap-4 font-semibold p-4">
 				<a class="items-center content-center flex" href="/blog">Blog</a>
 				<a class="items-center content-center flex" href="/snippets">Snippets</a>
 				<a class="items-center content-center flex" href="/projects">Projects</a>
 			</div>
-			<div class="border-l border-accent p-4">
+			<div class="sm:border-l sm:border-accent p-4">
 				<LightSwitch
 					fillLight="fill-surface-500"
 					fillDark="fill-surface-500"
